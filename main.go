9d871daad783c7970e60753afafb533b9577a96a
Angel Motta <angel.motta@utec.edu.pe>
2023-03-18 13:13:55 -0500

    feat: test connection ping pong

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
 
 func main() {
 	fmt.Println("Golang with in-memory database")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping(ctx).Result()
+	fmt.Println(pong, err)
 }
